d3f2c9793d9b3028d35d61650029a5b5d9d8adaf
yungProg <gboakye007@outlook.com>
2025-12-03 18:43:42 +0000

    user's show page lists all created events by the user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,18 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @events = Event.all
+  end
+
+  def show
+    # @event = Event.where(creator_id: event_params[:creator_id])
+    @event = current_user.created_events
+  end
+
+
+  private
+
+  def event_params
+    params.expect(event: [ :theme, :event_date, :venue, :creator_id ])
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,3 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+  belongs_to :creator, class_name: "User", inverse_of: "created_events"
 end
